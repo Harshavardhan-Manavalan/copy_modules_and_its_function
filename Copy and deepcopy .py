5a8a71ea16d4124427bbd56dc4493b926253674d
Harshavardhan-Manavalan <163420938+Harshavardhan-Manavalan@users.noreply.github.com>
2024-03-28 00:48:26 +0530

    Add files via upload
    added copy modules eg

---
+++ Copy and deepcopy .py
@@ -0,0 +1,10 @@
+import copy
+
+name = [34, 6, 8, 'ram', ['sam', 'hk', [0, 8], 7]]
+ram = copy.deepcopy(name)
+name.append(6)
+name[3] = "tom"
+name[4][0] = "ramesh"
+name[4][2][1] = "new"
+print(name)
+print(ram)
