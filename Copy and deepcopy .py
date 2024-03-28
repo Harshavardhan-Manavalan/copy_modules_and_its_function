7039c51470fcf2d37f452b7c871acba6635ab916
Harshavardhan-Manavalan <163420938+Harshavardhan-Manavalan@users.noreply.github.com>
2024-03-29 01:28:13 +0530

    Update Copy and deepcopy .py
    added code

--- Copy and deepcopy .py
+++ Copy and deepcopy .py
@@ -1,7 +1,8 @@
 import copy
 
 name = [34, 6, 8, 'ram', ['sam', 'hk', [0, 8], 7]]
-ram = copy.deepcopy(name)
+#ram = copy.copy(name)
+ram = copy.deepcopy(name) 
 name.append(6)
 name[3] = "tom"
 name[4][0] = "ramesh"
